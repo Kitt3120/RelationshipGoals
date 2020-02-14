b07033f8612c5b47f669540103d81ec4eba204d8
Kitt3120 <Kitt3120@aol.de>
2020-02-15 00:08:00 +0100

    Added IO functionality
    Added IDataProvider and IDataWriter interfaces
    Added FileDataProvider and WebContentProvider implementations
    Added FileDataWriter implementation
    Added static IOHandlers holder class

---
+++ IO/DataProvider/Implementation/WebContentProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationshipGoals.IO.DataProvider.Implementation
+{
+    internal class WebContentProvider : IDataProvider
+    {
+        public T Provide<T>(string key, string[] options = null)
+        {
+            using (WebClient webClient = new WebClient())
+                return (T)Convert.ChangeType(webClient.DownloadString(key), typeof(T));
+        }
+
+        public async Task<T> ProvideAsync<T>(string key, string[] options = null)
+        {
+            using (WebClient webClient = new WebClient())
+                return (T)Convert.ChangeType(await webClient.DownloadStringTaskAsync(key), typeof(T));
+        }
+    }
+}
---
+++ IO/IOHandlers.cs
@@ -0,0 +1,11 @@
+﻿using RelationshipGoals.IO.DataProvider.Implementation;
+using RelationshipGoals.IO.DataWriter.Implementation;
+
+namespace RelationshipGoals.IO
+{
+    public static class IOHandlers
+    {
+        public static FileDataProvider FileDataProvider { get; } = new FileDataProvider();
+        public static FileDataWriter FileDataWriter { get; } = new FileDataWriter();
+    }
+}

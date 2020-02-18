2fe743c25fcfa05c75525e822928abaed42b8654
Kitt3120 <Kitt3120@aol.de>
2020-02-18 20:02:52 +0100

    Changed settings not valid PopUp on startup
    PopUp now has formRelationshipGoals as parent and pops up in the center of it.

--- RelationshipGoals/Program.cs
+++ RelationshipGoals/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 using System.Windows.Forms;
 
 namespace RelationshipGoals
@@ -15,21 +16,25 @@ namespace RelationshipGoals
     {
         public static IServiceProvider ServiceProvider { get; private set; }
 
+        public delegate void StartedHandler();
+
+        public static event StartedHandler Ready;
+
         /// <summary>
         /// Der Haupteinstiegspunkt für die Anwendung.
         /// </summary>
         [STAThread]
         private static void Main()
+
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             FormRelationshipGoals formRelationshipGoals = new FormRelationshipGoals();
             formRelationshipGoals.Show();
-
             if (!SQLHandler.Check(Settings.Default.SQL_Server_Address, Settings.Default.SQL_Server_Schema, Settings.Default.SQL_Server_Login, Settings.Default.SQL_Server_Password))
             {
-                MessageBox.Show("Your SQL settings are not valid! Please provide valid SQL settings in the following window.");
+                MessageBox.Show(formRelationshipGoals, "Your SQL settings are not valid! Please provide valid SQL settings in the following window.");
                 FormSettings formSettings = new FormSettings();
                 formSettings.ShowDialog(formRelationshipGoals);
                 while (formSettings.Visible) //Wait until valid settings have been provided before building ServiceProvider
@@ -38,6 +43,7 @@ namespace RelationshipGoals
 
             BuildServiceProvider();
 
+            Ready.Invoke();
             Application.Run(formRelationshipGoals);
         }
 

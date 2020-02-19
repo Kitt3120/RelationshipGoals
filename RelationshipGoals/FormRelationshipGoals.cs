1a4da9f3fa4753c14b579b0bdadfce0b9995bcba
Kitt3120 <Kitt3120@aol.de>
2020-02-19 19:27:46 +0100

    Loading data into DataGridView
    FormRelationshipGoals now loads the GoalManager data into the grid on start

--- RelationshipGoals/FormRelationshipGoals.cs
+++ RelationshipGoals/FormRelationshipGoals.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using RelationshipGoals.Goals;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace RelationshipGoals
@@ -21,8 +22,15 @@ namespace RelationshipGoals
             {
                 int columnId = dataGridView.Columns.Add(goalTree.ID.ToString(), goalTree.Title);
 
-                foreach (Goal goal in goalTree.Goals.orde)
+                foreach (KeyValuePair<int, Goal> pair in goalTree.Ordered())
                 {
+                    while (dataGridView.Rows.Count <= pair.Key)
+                        dataGridView.Rows.Add();
+
+                    DataGridViewRow row = dataGridView.Rows[pair.Key];
+                    if (row == null)
+                        row = dataGridView.Rows[dataGridView.Rows.Add()];
+                    row.Cells[columnId] = new DataGridViewTextBoxCell() { Value = pair.Value.Title };
                 }
             }
         }

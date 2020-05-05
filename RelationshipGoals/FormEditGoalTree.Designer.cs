1049249e465170e1a58a9165af83ac2a0c3a4f40
Kitt3120 <Kitt3120@aol.de>
2020-05-05 15:51:46 +0200

    Added FormEditGoalTree
    Added FormEditGoalTree

---
+++ RelationshipGoals/FormEditGoalTree.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace RelationshipGoals
+{
+    partial class FormEditGoalTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEditGoalTree));
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.groupBoxProperties = new System.Windows.Forms.GroupBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.groupBoxGoals = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.groupBoxProperties.SuspendLayout();
+            this.groupBoxGoals.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDescription.BackColor = System.Drawing.Color.Plum;
+            this.textBoxDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxDescription.Location = new System.Drawing.Point(222, 32);
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(282, 20);
+            this.textBoxDescription.TabIndex = 7;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.AutoSize = true;
+            this.labelDescription.Location = new System.Drawing.Point(222, 16);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(60, 13);
+            this.labelDescription.TabIndex = 6;
+            this.labelDescription.Text = "Description";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.BackColor = System.Drawing.Color.Plum;
+            this.textBoxTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxTitle.Location = new System.Drawing.Point(6, 32);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(165, 20);
+            this.textBoxTitle.TabIndex = 5;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(6, 16);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(27, 13);
+            this.labelTitle.TabIndex = 4;
+            this.labelTitle.Text = "Title";
+            // 
+            // groupBoxProperties
+            // 
+            this.groupBoxProperties.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxProperties.Controls.Add(this.buttonUpdate);
+            this.groupBoxProperties.Controls.Add(this.textBoxTitle);
+            this.groupBoxProperties.Controls.Add(this.textBoxDescription);
+            this.groupBoxProperties.Controls.Add(this.labelTitle);
+            this.groupBoxProperties.Controls.Add(this.labelDescription);
+            this.groupBoxProperties.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxProperties.Name = "groupBoxProperties";
+            this.groupBoxProperties.Size = new System.Drawing.Size(510, 92);
+            this.groupBoxProperties.TabIndex = 8;
+            this.groupBoxProperties.TabStop = false;
+            this.groupBoxProperties.Text = "Properties";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonUpdate.BackColor = System.Drawing.Color.Plum;
+            this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUpdate.Location = new System.Drawing.Point(220, 63);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update Goal";
+            this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // groupBoxGoals
+            // 
+            this.groupBoxGoals.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxGoals.Controls.Add(this.dataGridView);
+            this.groupBoxGoals.Location = new System.Drawing.Point(12, 110);
+            this.groupBoxGoals.Name = "groupBoxGoals";
+            this.groupBoxGoals.Size = new System.Drawing.Size(510, 289);
+            this.groupBoxGoals.TabIndex = 9;
+            this.groupBoxGoals.TabStop = false;
+            this.groupBoxGoals.Text = "Goals";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Thistle;
+            this.dataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.Size = new System.Drawing.Size(498, 264);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // FormEditGoalTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
+            this.ClientSize = new System.Drawing.Size(534, 411);
+            this.Controls.Add(this.groupBoxGoals);
+            this.Controls.Add(this.groupBoxProperties);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(550, 450);
+            this.Name = "FormEditGoalTree";
+            this.Text = "Edit GoalTree";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormEditGoalTree_FormClosing);
+            this.Load += new System.EventHandler(this.FormEditGoalTree_Load);
+            this.groupBoxProperties.ResumeLayout(false);
+            this.groupBoxProperties.PerformLayout();
+            this.groupBoxGoals.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDescription;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.GroupBox groupBoxProperties;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.GroupBox groupBoxGoals;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
---
+++ RelationshipGoals/FormEditGoalTree.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using RelationshipGoals.Goal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RelationshipGoals
+{
+    public partial class FormEditGoalTree : Form
+    {
+        public GoalTree GoalTree { get; private set; }
+
+        /// <summary>
+        /// Whether or not the DataGridView has to be refreshed because a value has changed
+        /// </summary>
+        public bool NeedsRefresh { get; private set; }
+
+        public FormEditGoalTree(GoalTree goalTree)
+        {
+            InitializeComponent();
+            GoalTree = goalTree;
+            NeedsRefresh = false;
+        }
+
+        private void FormEditGoalTree_Load(object sender, EventArgs e)
+        {
+            textBoxTitle.Text = GoalTree.Title;
+            textBoxDescription.Text = GoalTree.Description;
+
+            LoadGoals();
+        }
+
+        private void LoadGoals()
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Columns.Clear();
+
+            dataGridView.Columns.Add("position", "Position");
+            dataGridView.Columns.Add("goal", "Goal");
+            dataGridView.Columns.Add("options", "Options");
+
+            for (int i = 0; i <= GoalTree.MaxPosition(); i++)
+                dataGridView.Rows.Add();
+
+            foreach (KeyValuePair<int, Goal.Goal> pair in GoalTree.GetPairs())
+            {
+                dataGridView[0, pair.Key] = new DataGridViewTextBoxCell() { Value = pair.Key };
+                dataGridView[1, pair.Key] = new DataGridViewTextBoxCell() { Value = pair.Value.Title };
+
+                Panel controlPanel = new Panel();
+                controlPanel.Controls.Add(new Button() { Text = "up", AutoSize = true, Anchor = AnchorStyles.Left, Dock = DockStyle.Left, FlatStyle = FlatStyle.Flat });
+                controlPanel.Controls.Add(new Button() { Text = "down", AutoSize = true, Anchor = AnchorStyles.Right, Dock = DockStyle.Right, FlatStyle = FlatStyle.Flat });
+                dataGridView.Controls.Add(controlPanel);
+
+                Rectangle displayRectangle = dataGridView.GetCellDisplayRectangle(2, pair.Key, true);
+                controlPanel.Location = displayRectangle.Location;
+                controlPanel.Size = displayRectangle.Size;
+
+                dataGridView.SizeChanged += (sender, eventArgs) =>
+                {
+                    displayRectangle = dataGridView.GetCellDisplayRectangle(2, pair.Key, true);
+                    controlPanel.Location = displayRectangle.Location;
+                    controlPanel.Size = displayRectangle.Size;
+                };
+            }
+        }
+
+        private void FormEditGoalTree_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!NeedsRefresh)
+                if (MessageBox.Show("Close without saving?", "Close GoalTree editing", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.No)
+                    e.Cancel = true;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            buttonUpdate.Enabled = false;
+
+            try
+            {
+                GoalTree.Title = textBoxTitle.Text.Trim();
+                NeedsRefresh = true; //As soon as the first query goes through, a UI refresh is needed
+                GoalTree.Description = textBoxDescription.Text.Trim();
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show($"Ein unbekannter Fehler ist aufgetreten.{Environment.NewLine}Die Einstellungen konnten nicht mit dem SQL-Server synchronisiert werden!", "SQL-Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            buttonUpdate.Enabled = true;
+        }
+    }
+}
